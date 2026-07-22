3890f60181e895f700981adfcde7c310422c8d55
ZERO0-OSS <3311682143@qq.com>
2026-07-22 22:39:46 +0800

    Day1: refactor calculator with functions

--- Python_basic/Calculator.py
+++ Python_basic/Calculator.py
@@ -1,11 +1,23 @@
-a,b=map(int,input("What are your numbers?").split())#map将字符列表全部转化为int类型，input读取整一行，split将字符串分裂
-print("What your operate?")##在Python中print自动换行，若不想要则print( ,end='')
-temp=input()
-if temp=='+':
-    print(a+b)
-elif temp=='-':
-    print(a-b)
-elif temp=='*':
-    print(a*b)
-elif temp=='/':
-    print(a/b)
+def add(a,b):
+    return a+b
+def delince(a,b):
+    return a-b
+def chne(a,b):
+    if b=='0':
+        return "Don't chu yu 0"
+    return a*b
+def chu(a,b):
+    return a/b
+
+if __name__=="__main__":
+    a,b=map(int,input("What are your numbers").split())
+    print("What is your operate")
+    temp=input()
+    if temp=="+":
+        print(add(a,b))
+    elif temp=="-":
+        print(delince(a,b))
+    elif temp=="/":
+        print(chu(a,b))
+    elif temp=="*":
+        print(chne(a,b))
